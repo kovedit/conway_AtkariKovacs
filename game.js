ad2b021ec228ddf8ebf0d15f4a80c17591b3e84c
kovedit <kedit0610@gmail.com>
2024-04-30 09:32:57 +0200

    932

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-let oszlopok = document.getElementById("oszlopok").value;
-let sorok = document.getElementById("sorok").value;
+const oszlopok = 6;
+const sorok = 6;
 
 const table = document.getElementsByTagName("table")[0];
 
@@ -11,6 +11,7 @@ for (let i = 0; i < sorok; i++) {
     row = document.createElement("tr");
     for (let j = 0; j < oszlopok; j++) {
         col = document.createElement("td");
+        col.innerText = i * j;
         row.append(col);
     }
     table.append(row);
